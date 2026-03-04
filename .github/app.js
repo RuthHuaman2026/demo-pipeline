c13a7067e004ca8312219f14712ca59e9f1995fd
RuthHuaman2026 <20241d022@khipu.edu.pe>
2026-03-04 06:04:22 +0100

    Remove comment from app.js

--- .github/app.js
+++ .github/app.js
@@ -1,6 +1,5 @@
-// app.js
 function saludar(nombre) {
     return `Hola, ${nombre}!`;
 }
-
 module.exports = saludar;
+4.  Si ya lo tiene, simplemente dale a **"Commit changes"** para que el "robot" (pipeline) se active.
