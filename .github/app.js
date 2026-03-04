2b4e1e0240ce03b7337e43eef239be17c00b720b
RuthHuaman2026 <20241d022@khipu.edu.pe>
2026-03-04 07:18:42 +0100

    Delete .github/app.js

--- .github/app.js
+++
@@ -1,5 +0,0 @@
-function saludar(nombre) {
-    return `Hola, ${nombre}!`;
-}
-module.exports = saludar;
-4.  Si ya lo tiene, simplemente dale a **"Commit changes"** para que el "robot" (pipeline) se active.
